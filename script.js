871e6bb20fe178f7d09fddf95197c95f8b8401c9
Jatin Dhiman <dhimanjatin003@gmail.com>
2024-08-16 17:36:38 +0530

    Adding code

---
+++ script.js
@@ -0,0 +1,87 @@
+const navDialog = document.getElementById('nav-dialog');
+function handleMenu(){
+    navDialog.classList.toggle('hidden')
+}
+/**
+ * <div class="mt-12 mb-12">
+        <!-- Upper Body -->
+        <div class="flex items-center justify-center mt-[300px] md:mt-8">
+            <!-- Left -->
+            <div class="mt-5">
+                <h1 class="text-6xl font-medium ">Lessons and insights</h1>
+                <h1 class="mt-3 text-6xl font-medium text-green-600">from 8 years</h1>
+                <h5 class="mt-4  text-gray-600">Where to grow business as a phoographer :site or sociial media</h5>
+            </div>
+            <!-- Right -->
+            <div>
+                <img src="https://rankoders.com/wp-content/uploads/elementor/thumbs/Illustration-1-qrherb5ei11mrmlc2nroqnu2kyp1l5tbxlzlwze1xc.png"
+                    class="ml-40" alt="">
+            </div>
+        </div>
+        <!-- Lower Part -->
+        <div>
+            <h1 class="text-center text-4xl font-medium text-zinc-600">Our Clients</h1>
+            <h5 class="mt-4 text-gray-600 text-center">We have been working with some fortune 500+ clients</h5>
+            <!-- Logos in row -->
+            <div class="flex justify-evenly ml-[120px] mt-4 mb-12">
+
+                <img class="w-20 h-15" src="./logos/—Pngtree—n hexagon black logo vector_4153371.png" alt="">
+                <img class="w-20 h-15" src="./logos/—Pngtree—n hexagon black logo vector_4153371.png" alt="">
+                <img class="w-20 h-15" src="./logos/—Pngtree—n hexagon black logo vector_4153371.png" alt="">
+                <img class="w-20 h-15" src="./logos/—Pngtree—n hexagon black logo vector_4153371.png" alt="">
+                <img class="w-20 h-15" src="./logos/—Pngtree—n hexagon black logo vector_4153371.png" alt="">
+                <img class="w-20 h-15" src="./logos/—Pngtree—n hexagon black logo vector_4153371.png" alt="">
+                <img class="w-20 h-15" src="./logos/—Pngtree—n hexagon black logo vector_4153371.png" alt="">
+            </div>
+            <h1 class="text-center text-4xl font-medium text-zinc-600 mb-6">Manage Your entire community <br>in a single
+                system</h1>
+            <h5 class="mt-4 text-gray-600 text-center mb-8">Who is Nextcent suitable for"</h5>
+            <!-- 3-cards -->
+            <div class="flex justify-evenly ">
+                <div class="border border-gray-100 w-60 h-auto p-4 shadow-lg shadow-gray-500/50 ">
+                    <img src="./logos/group.png" alt="" class="ml-6 w-40 ">
+                    <h2 class="text-center text-2xl font-medium text-zinc-600">Mmebership Organisation</h2>
+                    <p class="mt-4 ml-4 text-gray-600 text-sm leading-tight ">Our mebership management software provides
+                        full automation of membership renewals and payments</p>
+
+                </div>
+                <div class="border border-gray-100 w-64 h-auto p-4 shadow-lg shadow-gray-500/50">
+                    <img src="https://s.tmimgcdn.com/scr/800x500/287900/factory-building-modern-industrial-logo-design-vector_287909-original.jpg"
+                        class="ml-6 w-48 h-36" alt="">
+                    <h2 class="text-center text-2xl font-medium text-zinc-600">National Assoscitons</h2>
+                    <p class="mt-4 ml-4 text-gray-600 text-sm leading-tight">Our membership management software provides
+                        full automation of membership renewals and payments</p>
+                </div>
+                <div class="border border-gray-100 w-60 h-auto p-4 shadow-lg shadow-gray-500/50">
+                    <img src="./logos/group.png" alt="" class="ml-6 w-40">
+                    <h2 class="text-center text-2xl font-medium text-zinc-600">Membership Organisation</h2>
+                    <p class="mt-4 ml-4 text-gray-600 text-sm leading-tight ">Our mebership management software provides
+                        full automation of membership renewals and payments</p>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- Body -->
+    <div class="flex justify-center m-4 ml-[5px] mb-8">
+        <!-- Left Image -->
+        <div class="w-[500px] h-auto">
+            <img src="https://assets.softr-files.com/applications/b430af18-f433-48b4-9ab7-9f9fb605b3a8/assets/db3c4cad-4ac8-47c6-b9f3-1d0d3235bf5b.png"
+                alt="" class="">
+        </div>
+        <!-- Content Right -->
+        <div class="w-[500px] mt-20 ml-8">
+            <h2 class="text-center text-4xl font-medium text-zinc-600 mt-4 mb-8">The unseen of spending three years at
+                Pixelgrade</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam explicabo libero maxime expedita, modi
+                nesciunt laudantium ratione rem? Soluta illum alias similique, quam quos perferendis praesentium
+                nostrum, laborum omnis, quas sed officia quod vitae fuga vol Et unde inventore, ducimus reiciendis enim
+                amet provident.</p>
+            <button class="border border-green-100 py-[10px] px-4 bg-green-500 text-white mt-6 ml-1 w-32   ">Learn more</button>
+        </div>
+
+    </div>
+    <!-- Helping -->
+
+ */
